5f9e72feebb213c653a64d3da9e14933a3daf71d
Sebastian <crsebastian_el_capo_2016@hotmail.com>
2025-04-14 22:05:32 -0400

    create structure GUI

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
+            openFileDialog1 = new OpenFileDialog();
+            saveFileDialog1 = new SaveFileDialog();
+            menuStrip1 = new MenuStrip();
+            archivoToolStripMenuItem = new ToolStripMenuItem();
+            cargarToolStripMenuItem = new ToolStripMenuItem();
+            guardarToolStripMenuItem = new ToolStripMenuItem();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            inferirBtn = new Button();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { archivoToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(655, 28);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            archivoToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { cargarToolStripMenuItem, guardarToolStripMenuItem });
+            archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            archivoToolStripMenuItem.Size = new Size(73, 24);
+            archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // cargarToolStripMenuItem
+            // 
+            cargarToolStripMenuItem.Name = "cargarToolStripMenuItem";
+            cargarToolStripMenuItem.Size = new Size(145, 26);
+            cargarToolStripMenuItem.Text = "Cargar";
+            // 
+            // guardarToolStripMenuItem
+            // 
+            guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            guardarToolStripMenuItem.Size = new Size(145, 26);
+            guardarToolStripMenuItem.Text = "Guardar";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(73, 64);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(484, 375);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(73, 497);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(244, 27);
+            textBox2.TabIndex = 2;
+            // 
+            // inferirBtn
+            // 
+            inferirBtn.Location = new Point(463, 495);
+            inferirBtn.Name = "inferirBtn";
+            inferirBtn.Size = new Size(94, 29);
+            inferirBtn.TabIndex = 3;
+            inferirBtn.Text = "inferir";
+            inferirBtn.UseVisualStyleBackColor = true;
+            inferirBtn.Click += inferirBtn_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(655, 573);
+            Controls.Add(inferirBtn);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(menuStrip1);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private OpenFileDialog openFileDialog1;
+        private SaveFileDialog saveFileDialog1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem archivoToolStripMenuItem;
+        private ToolStripMenuItem cargarToolStripMenuItem;
+        private ToolStripMenuItem guardarToolStripMenuItem;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button inferirBtn;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,19 @@
-namespace ProtoProlog
+namespace ProtoProlog;
+
+public partial class Form1 : Form
 {
-    public partial class Form1 : Form
+    public Form1()
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+        Console.WriteLine("Hola mundo");
+    }
+
+    private void inferirBtn_Click(object sender, EventArgs e)
+    {
+        // inferir boton
     }
 }
